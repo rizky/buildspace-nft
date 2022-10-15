c5669a3e6edf2a842ecdb4cdcc30c2e5fad97f77
Rizky Ario <rizkyario@me.com>
2022-10-15 21:28:30 +0200

    Add gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 
 import myEpicNft from './utils/MyEpicNFT.json';
 
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'rizkyario';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
